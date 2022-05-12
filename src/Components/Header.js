368a3fa7da6fd26d5defefa5b003bce9da8ed9a2
Jorge Tadeu <jsbatalha@gmail.com>
2022-05-12 07:37:34 -0300

    Layout do Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,8 +6,13 @@ export default function Header(){
             <Top>
                 <ion-icon name="menu-outline"></ion-icon>
                 <Logo></Logo>
-                <ion-icon name="person-circle-outline"></ion-icon>
-                <ion-icon name="cart-outline"></ion-icon>
+                <Right>
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                    <Cart>
+                        <ion-icon name="cart-outline"></ion-icon>
+                        <p>1</p>
+                    </Cart>
+                </Right>
             </Top>
             <Search>
                 <SearchBar></SearchBar>
@@ -19,17 +24,72 @@ export default function Header(){
 
 const Head = styled.header`
     width: 100%;
-    height: 10%;
+    height: 15%;
 `
 const Top = styled.div`
+    width: 100%;
+    height: 70%;
+    background-color: #064973;
+    
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
+    box-sizing: border-box;
+    padding: 5%;
+
+    ion-icon{
+        font-size: 30px;
+        color: #F2D5C4;
+    }
 `
 const Logo = styled.img`
 
 `
-const Search = styled.div`
+const Right = styled.div`
+    width:auto;
+    height: auto;
 
+    display: flex;
+`
+const Cart = styled.div`
+    position: relative;
+    p{
+        width: 12px;
+        height: 12px;
+
+        position: absolute;
+        top: 0;
+        right: 0;
+
+        font-size: 10px;
+        
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        background-color: #F2D5C4;
+        color: #064973;
+
+        border-radius: 50%;
+    }
+`
+const Search = styled.div`
+    width: 100%;
+    height: 30%;
+
+    background-color: #C4C4C4;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    ion-icon{
+        font-size: 25px;
+        color: #064973;
+    }
 `
 const SearchBar = styled.input`
-
+    width: 80%;
+    height: 50%;
 `
