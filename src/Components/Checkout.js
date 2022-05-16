a48c9e3b50e0836abb841e26725d9100b2e7eef3
Jorge Tadeu <jsbatalha@gmail.com>
2022-05-16 11:21:06 -0300

    Select state

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,55 +1,122 @@
 import styled from "styled-components";
 import Logo from "../Midia/Savant-logo.svg"
-import {useState, useEffect} from "react"
+import {useState} from "react"
 import { useCart } from "../contexts/UserContext";
 
 export default function Checkout(){
-    const [state,setState] = useState(false);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phone, setPhone] = useState("");
 
-    useEffect(() => {
-        console.log(state);
-        toggle(state);
-    }, [state])
+    const [street, setStreet] = useState("");
+    const [number, setNumber] = useState("");
+    const [complement, setComplement] = useState("");
+    const [district, setDistricst] = useState("");
+    const [city, setcity] = useState("");
+    const [zipCode, setZipCode] = useState("");
+    const [state, setState] = useState("");
+
+    const [cardNumber, setCardNumber] = useState("");
+    const [cardName, setCardName] = useState("");
+    const [valid, setValid] = useState("");
+    const [CVV, setCVV] = useState("");
+
+    const lastFour = cardNumber.substr(cardNumber.length - 4);
+
+    const {cart} = useCart();
+
+    let totalPrice = 0;
+
+    cart.map(product => {totalPrice += parseFloat(product.price)});
+
+    const orderInfo ={
+        items: cart,
+        total: totalPrice,
+        deliverTo: `${firstName} ${lastName}`,
+        contact: phone,
+        street: street,
+        number: number,
+        complement: complement,
+        district: district,
+        city: city,
+        ZIP: zipCode,
+        state: state,
+
+        paymentMethod: "card",
+        card: lastFour
+    }
+
+    function postOrder(){
+        console.log(orderInfo);
+    }
+
     return(
         <Page>
             <header> <img src={Logo}/></header>
 
-            <Products>
-                <ion-icon name="chevron-down-circle-outline" onClick={() => {(state === false) ? setState(true) : setState(false)}}></ion-icon>
-                <p>Ver detalhes do pedido</p>
-                <p></p>
-            </Products>
+            <CartData>
+
+            </CartData>
 
             <ShippingInfo>
                 <h2>Dados do destinatário</h2>
                 <form>
-                    <input placeholder="Nome"/>
-                    <input placeholder="Sobrenome"/>
-                    <input placeholder="Telefone"/>
+                    <input placeholder="Nome" onChange={(e) => {setFirstName(e.target.value)}}/>
+                    <input placeholder="Sobrenome" onChange={(e) => {setLastName(e.target.value)}}/>
+                    <input placeholder="Telefone" onChange={(e) => {setPhone(e.target.value)}}/>
                 </form>
             </ShippingInfo>
             <Adress>
                 <h2>Endereço do destinatário</h2>
                 <form>
-                    <input placeholder="Endereço"/>
-                    <input placeholder="Número"/>
-                    <input placeholder="Complemento"/>
-                    <input placeholder="Bairro"/>
-                    <input placeholder="Cidade"/>
-                    <input placeholder="CEP"/>
-                    <input placeholder="Estado"/>
+                    <input placeholder="Endereço" onChange={(e) => {setStreet(e.target.value)}}/>
+                    <input placeholder="Número" onChange={(e) => {setNumber(e.target.value)}}/>
+                    <input placeholder="Complemento" onChange={(e) => {setComplement(e.target.value)}}/>
+                    <input placeholder="Bairro" onChange={(e) => {setDistricst(e.target.value)}}/>
+                    <input placeholder="Cidade" onChange={(e) => {setcity(e.target.value)}}/>
+                    <input placeholder="CEP" onChange={(e) => {setZipCode(e.target.value)}}/>
+                    <select onChange={(e) => {setState(e.target.value)}}>
+                        <option value="">Selecionar Estado</option>
+                        <option value="Acre">Acre</option>
+                        <option value="Alagoas">Alagoas</option>
+                        <option value="Amapá">Amapá</option>
+                        <option value="Amazonas">Amazonas</option>
+                        <option value="Bahia">Bahia</option>
+                        <option value="Ceará">Ceará</option>
+                        <option value="Distrito Federal">Distrito Federal</option>
+                        <option value="Espírito Santo">Espírito Santo</option>
+                        <option value="Goiás">Goiás</option>
+                        <option value="Maranhão">Maranhão</option>
+                        <option value="Mato Grosso">Mato Grosso</option>
+                        <option value="Mato Grosso do Sul">Mato Grosso do Sul</option>
+                        <option value="Minas Gerais">Minas Gerais</option>
+                        <option value="Pará">Pará</option>
+                        <option value="Paraíba">Paraíba</option>
+                        <option value="Paraná">Paraná</option>
+                        <option value="Pernambuco">Pernambuco</option>
+                        <option value="Piauí">Piauí</option>
+                        <option value="Rio de Janeiro">Rio de Janeiro</option>
+                        <option value="Rio Grande do Norte">Rio Grande do Norte</option>
+                        <option value="Rio Grande do Sul">Rio Grande do Sul</option>
+                        <option value="Rondônia">Rondônia</option>
+                        <option value="Rorâima">Rorâima</option>
+                        <option value="Santa Catarina">Santa Catarina</option>
+                        <option value="São Paulo">São Paulo</option>
+                        <option value="Sergipe">Sergipe</option>
+                        <option value="Tocantins">Tocantins</option>
+                    </select>
                 </form>
             </Adress>
             <Payment>
                 <h2>Pagamento</h2>
                 <form>
-                    <input placeholder="Número do Cartão"/>
-                    <input placeholder="Nome no Cartão"/>
-                    <input placeholder="Vencimento"/>
-                    <input placeholder="CVV"/>
+                    <input placeholder="Número do Cartão" onChange={(e) => {setCardNumber(e.target.value)}}/>
+                    <input placeholder="Nome no Cartão" onChange={(e) => {setCardName(e.target.value)}}/>
+                    <input placeholder="Vencimento" onChange={(e) => {setValid(e.target.value)}}/>
+                    <input placeholder="CVV" onChange={(e) => {setCVV(e.target.value)}}/>
                 </form>
             </Payment>
-            <button>Finalizar</button>
+            <button onClick={postOrder}>Finalizar</button>
         </Page>
     )
 }
@@ -62,7 +129,8 @@ const Page = styled.main`
     width: 100%;
     height: 100%;
 
-    position: relative;
+    display:flex;
+    flex-direction: column;
 
     header{
         width: 100%;
@@ -88,6 +156,18 @@ const Page = styled.main`
         border: 2px solid #C4C4C4;
     }
 
+    select{
+        width: 90%;
+        height: 40px;
+
+        margin: 5px 0;
+
+        border-radius: 10px;
+        border: 2px solid #C4C4C4;
+
+        background-color: #FFFFFF
+    }
+
     section{
         margin: 10px 0;
     }
@@ -103,27 +183,11 @@ const Page = styled.main`
 
         border-radius: 40px;
 
-        position: absolute;
-
-        left: 25%;
+        margin: 10px auto;
     }
 `
-const Products = styled.div`
-    width: 100%;
-    height: ${toggle};
+const CartData = styled.div`
 
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    border: 1px solid #C4C4C4;
-
-    box-sizing: border-box;
-    padding: 10px;
-
-    ion-icon{
-        font-size: 20px;
-    }
 `
 const ShippingInfo = styled.section`
     width: 100%;
