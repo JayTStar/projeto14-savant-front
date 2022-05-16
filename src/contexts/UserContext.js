c71916e19fa8a2a41f1b5fe3f2facd67114f58cc
Jorge Tadeu <jsbatalha@gmail.com>
2022-05-16 00:37:01 -0300

    Apagando itens do carrinho

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,18 +5,7 @@ export const UserData = createContext();
 export default function Provider({ children }){
     const [userInfo, setUserInfo] = useState("");
     const [token, setToken] = useState("");
-    const [cart, setCart] = useState([
-        {title:"1 Harry Potter e a pedra filosofal", price:"24.30", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},
-        {title:"2 Harry Potter e a pedra filosofal", price:"24.10", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},
-        {title:"3 Harry Potter e a pedra filosofal", price:"24.50", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},
-        {title:"4 Harry Potter e a pedra filosofal", price:"24.80", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},{title:"1 Harry Potter e a pedra filosofal", price:"24.30", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},
-        {title:"2 Harry Potter e a pedra filosofal", price:"24.10", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},
-        {title:"3 Harry Potter e a pedra filosofal", price:"24.50", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},
-        {title:"4 Harry Potter e a pedra filosofal", price:"24.80", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},{title:"1 Harry Potter e a pedra filosofal", price:"24.30", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},
-        {title:"2 Harry Potter e a pedra filosofal", price:"24.10", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},
-        {title:"3 Harry Potter e a pedra filosofal", price:"24.50", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."},
-        {title:"4 Harry Potter e a pedra filosofal", price:"24.80", author:"J.K. Rowling", genre:"aventura", synopsis:"Deveria ser um livro só para o público infanto-juvenil. Mas, no mundo ...", image:"https://images-na.ssl-images-amazon.com/images/I/518mqZ7A31L._SX332_BO..."}
-    ]);
+    const [cart, setCart] = useState([]);
 
     return(
         <UserData.Provider value={{userInfo, setUserInfo, token, setToken, cart, setCart}}>
